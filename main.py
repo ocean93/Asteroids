caa7ed863879da25d7de93c840e8046ce4fec7bb
ocean93 <oceanmchappell@gmail.com>
2024-12-03 20:08:21 -0500

    Implement basic game loop with black screen

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        screen.fill("black")
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+main()
+
