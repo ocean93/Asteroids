8a4756f16a6a078c3a1cc35f9a244b641bc4c6e2
ocean93 <oceanmchappell@gmail.com>
2024-12-03 21:32:38 -0500

    updated with player triangle

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill("black")
 
+        player.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-main()
+if __name__ == "__main__":
+    main()
 
