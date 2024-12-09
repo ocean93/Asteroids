fc8cafeb8244e568ac1fe6968a3a8fc913260738
ocean93 <oceanmchappell@gmail.com>
2024-12-09 15:14:06 -0500

    updated circleshape

--- circleshape.py
+++ circleshape.py
@@ -16,5 +16,12 @@ class CircleShape(pygame.sprite.Sprite):
     def draw(self, screen):
         pass
 
+    def collision(self, other_shape):
+        distance = self.position.distance_to(other_shape.position)
+        if distance <= self.radius + other_shape.radius:
+            return True
+        else:
+            return False
+
     def update(self, dt):
         pass
